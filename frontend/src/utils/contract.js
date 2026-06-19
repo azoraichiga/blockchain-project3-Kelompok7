7d0dc950ab49ef56ec26d7f0c832cf43063b4187
fqhhusain <fqhhusain@gmail.com>
2026-06-19 09:59:21 +0700

    fix(web3): update CONTRACT_ADDRESS to localhost deploy

--- frontend/src/utils/contract.js
+++ frontend/src/utils/contract.js
@@ -15,7 +15,7 @@
 //     yang bisa membuat claim gagal walau reward tersedia.
 // =============================================================
 
-export const CONTRACT_ADDRESS = "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512";
+export const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 export const CONTRACT_ABI = [
   // --- READ ---
